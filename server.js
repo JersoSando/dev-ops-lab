0811aa5527a5b124142361d74338cdea174d7b8f
Jerso Sando <jerso.sando@gmail.com>
2022-01-27 14:21:16 -0700

    rollbar try

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const app = express();
 // app.use(express.jason())
 app.use(cors());
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '8f09df17f607463d9fe058bfb182aec3',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 // app.use("/styles", express.static(path.join(__dirname, "./styles.css")));
 
@@ -22,7 +32,10 @@ app.get("/", function(req, res) {
 
 // process.env.PORT is HEROKU'S responsibility. They will provide the PORT number on deployment.
 // The second port number is for local development purposes only.
+app.use(rollbar.errorHandler());
+
 const port = process.env.PORT || 5001;
+
 
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
