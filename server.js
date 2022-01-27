4cd31bdd155ec7cbd8de56b5da685e075c322c71
Jerso Sando <jerso.sando@gmail.com>
2022-01-27 13:46:04 -0700

    server fixed

--- server.js
+++ server.js
@@ -4,3 +4,25 @@ const path = require ('path')
 
 app.use(express.jason())
 app.use(cors())
+
+
+// app.use("/styles", express.static(path.join(__dirname, "./styles.css")));
+
+app.use("/js", express.static(path.join(__dirname, "./server.js")));
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+// app.get("/styles", function(req, res) {
+//     res.sendFile(path.join(__dirname, "./styles.css"));
+// });
+
+
+// process.env.PORT is HEROKU'S responsibility. They will provide the PORT number on deployment.
+// The second port number is for local development purposes only.
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+});
