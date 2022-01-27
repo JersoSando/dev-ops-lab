744ecaa25bf332959fc7658df3df37cc98198e93
Jerso Sando <jerso.sando@gmail.com>
2022-01-27 13:53:52 -0700

    deploy again

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const app = express()
+const cors = require('cors')
 const path = require ('path')
 
-app.use(express.jason())
-app.use(cors())
+const app = express();
+// app.use(express.jason())
+app.use(cors());
 
 
 // app.use("/styles", express.static(path.join(__dirname, "./styles.css")));
 
-app.use("/js", express.static(path.join(__dirname, "./server.js")));
+// app.use("/js", express.static(path.join(__dirname, "./server.js")));
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "./index.html"));
