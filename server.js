4686d28b5d242da74d540cbf4ee13986c5e1ffee
Jerso Sando <jerso.sando@gmail.com>
2022-01-27 14:37:51 -0700

    rollbar again

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const cors = require('cors')
 const path = require ('path')
 
 const app = express();
-// app.use(express.jason())
 app.use(cors());
 
 // include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
 var rollbar = new Rollbar({
-  accessToken: '8f09df17f607463d9fe058bfb182aec3',
+  accessToken: 'faa7402d425046a69dd848a183abc21a',
   captureUncaught: true,
   captureUnhandledRejections: true,
 })
@@ -17,17 +16,10 @@ var rollbar = new Rollbar({
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
 
-// app.use("/styles", express.static(path.join(__dirname, "./styles.css")));
-
-// app.use("/js", express.static(path.join(__dirname, "./server.js")));
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "./index.html"));
 });
-
-// app.get("/styles", function(req, res) {
-//     res.sendFile(path.join(__dirname, "./styles.css"));
-// });
 
 
 // process.env.PORT is HEROKU'S responsibility. They will provide the PORT number on deployment.
